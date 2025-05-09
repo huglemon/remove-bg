56cc5d50840b57294382ee5ee44fcb4157473ebb
huglemon <hi@huglemon.com>
2025-05-09 10:42:46 +0800

    feat(layout): enhance layout with new icons and background pattern; improve footer structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { Toaster as RadixToaster } from "@/components/ui/toaster";
 import { Toaster } from "sonner";
 import Link from "next/link";
 import { ModeSwitch } from "@/components/mode-switch";
+import { LayoutGrid, Globe, Store } from "lucide-react";
 
 import { Geist, Geist_Mono } from "next/font/google";
 import localFont from "next/font/local";
@@ -47,7 +48,7 @@ export default function RootLayout({
           geistSans.variable,
           geistMono.variable,
           dingTalkFont.variable,
-          `flex min-h-screen flex-col bg-gray-100 text-gray-900 antialiased`,
+          `flex min-h-screen flex-col bg-[length:30px_30px] bg-[linear-gradient(45deg,#eaeaea_25%,transparent_25%,transparent_75%,#eaeaea_75%,#eaeaea),linear-gradient(45deg,#eaeaea_25%,transparent_25%,transparent_75%,#eaeaea_75%,#eaeaea)] bg-[position:0_0,15px_15px] text-gray-900 antialiased`,
         )}
       >
         <header className="relative py-6">
@@ -62,7 +63,7 @@ export default function RootLayout({
         <main className="grow overflow-hidden">{children}</main>
         <RadixToaster />
         <Toaster position="top-center" richColors />
-        <footer className="mx-auto mt-14 flex w-full max-w-7xl items-center justify-between px-4 py-6 md:mt-0">
+        <footer className="mx-auto mt-14 flex w-full max-w-7xl flex-col items-center justify-between gap-4 px-4 py-6 md:mt-0 md:flex-row md:gap-0">
           <p className="text-xs text-gray-300 md:text-sm">
             <Link href="https://inwind.cn" target="_blank">
               inWind Overseas
@@ -72,18 +73,23 @@ export default function RootLayout({
 
           <div className="flex items-center gap-2 md:gap-3">
             <a
+              title="外贸独立站"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-flex items-center gap-1 rounded-lg border border-gray-250 bg-white px-2 py-1.5 text-xs text-gray-300 shadow transition hover:bg-white/75 md:rounded-xl md:px-4 md:text-sm"
+              href="https://whenthey.work"
+            >
+              <Store className="size-3" />
+              外贸独立站建站
+            </a>
+            <a
               title="快速查询目标地区是否处于工作时间"
               target="_blank"
               rel="noopener noreferrer"
               className="inline-flex items-center gap-1 rounded-lg border border-gray-250 bg-white px-2 py-1.5 text-xs text-gray-300 shadow transition hover:bg-white/75 md:rounded-xl md:px-4 md:text-sm"
               href="https://whenthey.work"
             >
-              <Image
-                src="/whentheywork.svg"
-                alt="WhenThey.Work"
-                width={16}
-                height={16}
-              />
+              <Globe className="size-3" />
               WhenThey.Work
             </a>
             <a
@@ -92,7 +98,7 @@ export default function RootLayout({
               rel="noopener noreferrer"
               className="inline-flex items-center gap-1 rounded-lg border border-gray-250 bg-white px-2 py-1.5 text-xs text-gray-300 shadow transition hover:bg-white/75 md:rounded-xl md:px-4 md:text-sm"
             >
-              <XIcon className="size-3" />
+              <LayoutGrid className="size-3" />
               外贸工具导航
             </a>
           </div>
