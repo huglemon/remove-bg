fde7035c0f88a9ba64aa86d39ace865a4897830d
huglemon <hi@huglemon.com>
2026-01-19 12:09:29 +0800

    ♻️ refactor: 更新 Cross-Origin-Embedder-Policy 配置为 credentialless

--- next.config.js
+++ next.config.js
@@ -29,7 +29,7 @@ const nextConfig = {
           },
           {
             key: "Cross-Origin-Embedder-Policy",
-            value: "require-corp",
+            value: "credentialless",
           },
         ],
       },
